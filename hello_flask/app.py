120a58503580ee301dbb74588d5093850be44461
korbenk <korben@kirscht.com>
2016-03-20 20:28:55 -0700

    Initial

---
+++ hello_flask/app.py
@@ -0,0 +1,14 @@
+#!/Users/korbenk/octt/flask/hello_flask/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello, Flask!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
